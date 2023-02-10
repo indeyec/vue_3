927d277c0253ddc29f2b14e856e819b8cd50bb33
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-02-10 23:40:47 +0700

    css

--- js/main.js
+++ js/main.js
@@ -1,127 +1,6 @@
 let eventBus = new Vue()
 
-Vue.component('note', {
-    props: {
-        types: ''
-    },
-    data() {
-        return {
-            notes: [],
-            comment: '',
-        }
-    },
-    template: `
-        <div>
-            <div class="m-3" v-for="note in notes" v-show="note.type == types ">
-                <div 
-                    class="p-3 border card-enter" 
-                    :class="{ 
-                        'border-success': note.compliteInTime, 
-                        'border-danger': !note.compliteInTime && note.type == 'col-4', 
-                        'border-primary': !note.compliteInTime, 'border-warning': note.comment.length != 0,
-                    }"
-                >
-                    <h5>{{note.title}} ({{note.noteId}}) - <span style="color: red;" @click="deleteNote(note.noteId)">X</span></h5>
-                    <p>{{ note.description }}</p>
-                    <p><hr>
-                    Дата создания: {{ note.dateCreate }}<br>
-                    Дэдлайн: {{ note.dateDeadline }} - {{ note.compliteInTime }}<br>
-                    <span v-if="note.dateUpdate.length != 0">Дата изменения: {{ note.dateUpdate }}</span>
-                    <hr>
-                    </p>
-                    <p v-if="note.comment.length > 0">Комментарий:<br>{{ note.comment }}</p>
-                    <div v-if="note.type != 'col-4'">
-                        <span class="btn btn-warning" @click="noteUpdate(note)">Редактировать...</span>
-                        <div class="mt-2">
-                            <span class="btn btn-success" @click="changeType(note)">Перенести</span>
-                            <span v-if="note.type == 'col-3'" class="btn btn-danger" @click="comeBack(note)">Вернуть</span>
-                            <div class="mt-3" v-if="note.type == 'col-3'">
-                                <form>
-                                    <div class="form-floating mb-3">
-                                        <textarea class="form-control comeback" id="textarea" style="height: 200px; resize: none;" v-model="comment"></textarea>
-                                        <label for="textarea">Введите причину возврата:</label>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `,
-    
-    methods: {
-        deleteNote(id) {
-            for (note in this.notes) {
-                if (this.notes[note].noteId == id) {
-                    this.notes.splice(note, 1)
-                }
-            }
-        },
-        noteUpdate(note) {
-            eventBus.$emit('update-note', note)
-        },
-        changeType(note) {
-            if (note.type == 'col-1') {
-                note.type = 'col-2'
-            } else {
-                if (note.type == 'col-2') {
-                    note.type = 'col-3'
-                } else {
-                    if (note.type == 'col-3') {
-                        note.type = 'col-4'
-                        let dateComplite = new Date().toLocaleDateString().split('.')
-                        let dateDeadline = note.dateDeadline.split('.')
-                        note.dateComplite = dateComplite
-                        if ( Number(dateComplite[2]) >= Number(dateDeadline[2]) ) {
-                            if ( Number(dateComplite[1]) >= Number(dateDeadline[1]) ) {
-                                if ( Number(dateComplite[0]) >= Number(dateDeadline[0]) ) {
-                                    note.compliteInTime = false
-                                } else { note.compliteInTime = true }
-                            } else { note.compliteInTime = true }
-                        } else { note.compliteInTime = true }
-                    }
-                }
-            }
-        },
-        comeBack(note) {
-            if (this.comment ) {
-                note.comment = this.comment
-                note.type = 'col-2'
-                this.comment = ''
-            }
-        }
-    },
-    mounted() {
-        eventBus.$on('note-created', note => {
-            this.notes.push(note)
-        })
-        eventBus.$on('note-updated', note => {
-            for (el in this.notes) {
-                if (this.notes[el].noteId == note.noteId) {
-                    console.log(this.notes[el])
-                    this.notes[el].title = note.title
-                    this.notes[el].description = note.description
-                    this.notes[el].dateDeadline = note.dateDeadline
-                    this.notes[el].dateUpdate = note.dateUpdate
-                }
-            }
-        })
-    },
-})
-
 Vue.component('create-note', {
-    data() {
-        return {
-            noteId: 0,
-            title: '',
-            description: '',
-            dateDeadline: '',
-            note: '',
-            update: false,
-            errors: []
-        }
-    },
     template: `
         <div>
             <ul>
@@ -129,13 +8,13 @@ Vue.component('create-note', {
             </ul>
             <form class="d-flex flex-column mt-4 " @submit.prevent="createNote">
                 <fieldset>
-                    <input class="form-control mb-3" type="text" placeholder="Заголовок" v-model="title">
+                    <input class="form-control mb-3" type="text" placeholder="Name" v-model="title">
                     <div class="form-floating mb-3">
                         <textarea class="form-control" id="textarea" style="height: 200px; resize: none;" v-model="description"></textarea>
-                        <label for="textarea">Введите описание задачи здесь:</label>
+                        <label for="textarea">Opisanie:</label>
                     </div>
                     <div class="mb-4">
-                        <label class="color-white" for="deadline">Дэдлайн</label>
+                        <label class="color-white" for="deadline">Dead_line</label>
                         <input class="mt-3" name="deadline" type="date" min="2023-01-01" v-model="dateDeadline">
                     </div>
                     <input v-if="!update" class="btn btn-primary" type="submit" value="Создать">
@@ -222,8 +101,135 @@ Vue.component('create-note', {
             this.update = true
             this.openUpdateNote()
         })
-    }
+    },
+    data() {
+        return {
+            noteId: 0,
+            title: '',
+            description: '',
+            dateDeadline: '',
+            note: '',
+            update: false,
+            errors: []
+        }
+    },
 })
+
+
+
+
+Vue.component('note', {
+    props: {
+        types: ''
+    },
+    
+    template: `
+        <div>
+            <div class="m-3" v-for="note in notes" v-show="note.type == types ">
+                <div 
+                    class="p-3 border card-enter" 
+                    :class="{ 
+                        'border-success': note.compliteInTime, 
+                        'border-danger': !note.compliteInTime && note.type == 'col-4', 
+                        'border-primary': !note.compliteInTime, 'border-warning': note.comment.length != 0,
+                    }"
+                >
+                    <h5>{{note.title}} ({{note.noteId}}) - <span style="color: red;" @click="deleteNote(note.noteId)">X</span></h5>
+                    <p>{{ note.description }}</p>
+                    <p><hr>
+                    Дата создания: {{ note.dateCreate }}<br>
+                    Дэдлайн: {{ note.dateDeadline }} - {{ note.compliteInTime }}<br>
+                    <span v-if="note.dateUpdate.length != 0">Дата изменения: {{ note.dateUpdate }}</span>
+                    <hr>
+                    </p>
+                    <p v-if="note.comment.length > 0">Comments:<br>{{ note.comment }}</p>
+                    <div v-if="note.type != 'col-4'">
+                        <span class="btn btn-warning" @click="noteUpdate(note)">Redact</span>
+                        <div class="mt-2">
+                            <span class="btn btn-success" @click="changeType(note)">Next</span>
+                            <span v-if="note.type == 'col-3'" class="btn btn-danger" @click="comeBack(note)">Назад</span>
+                            <div class="mt-3" v-if="note.type == 'col-3'">
+                                <form>
+                                    <div class="form-floating mb-3">
+                                        <textarea class="form-control comeback" id="textarea" style="height: 200px; resize: none;" v-model="comment"></textarea>
+                                        <label for="textarea">Причина возврата:</label>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
+    
+    methods: {
+        deleteNote(id) {
+            for (note in this.notes) {
+                if (this.notes[note].noteId == id) {
+                    this.notes.splice(note, 1)
+                }
+            }
+        },
+        noteUpdate(note) {
+            eventBus.$emit('update-note', note)
+        },
+        changeType(note) {
+            if (note.type == 'col-1') {
+                note.type = 'col-2'
+            } else {
+                if (note.type == 'col-2') {
+                    note.type = 'col-3'
+                } else {
+                    if (note.type == 'col-3') {
+                        note.type = 'col-4'
+                        let dateComplite = new Date().toLocaleDateString().split('.')
+                        let dateDeadline = note.dateDeadline.split('.')
+                        note.dateComplite = dateComplite
+                        if ( Number(dateComplite[2]) >= Number(dateDeadline[2]) ) {
+                            if ( Number(dateComplite[1]) >= Number(dateDeadline[1]) ) {
+                                if ( Number(dateComplite[0]) >= Number(dateDeadline[0]) ) {
+                                    note.compliteInTime = false
+                                } else { note.compliteInTime = true }
+                            } else { note.compliteInTime = true }
+                        } else { note.compliteInTime = true }
+                    }
+                }
+            }
+        },
+        comeBack(note) {
+            if (this.comment ) {
+                note.comment = this.comment
+                note.type = 'col-2'
+                this.comment = ''
+            }
+        }
+    },
+    mounted() {
+        eventBus.$on('note-created', note => {
+            this.notes.push(note)
+        })
+        eventBus.$on('note-updated', note => {
+            for (el in this.notes) {
+                if (this.notes[el].noteId == note.noteId) {
+                    console.log(this.notes[el])
+                    this.notes[el].title = note.title
+                    this.notes[el].description = note.description
+                    this.notes[el].dateDeadline = note.dateDeadline
+                    this.notes[el].dateUpdate = note.dateUpdate
+                }
+            }
+        })
+    },
+    data() {
+        return {
+            notes: [],
+            comment: '',
+        }
+    },
+})
+
+
 
 let app = new Vue({
     el: '#app',
